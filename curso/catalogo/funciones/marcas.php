5c1e69e6173eb7af656863c3cb50afb5fa157fe5
Marcos <exegeses@gmail.com>
2023-08-02 22:20:06 -0300

    Proyecto integrador

--- curso/catalogo/funciones/marcas.php
+++ curso/catalogo/funciones/marcas.php
@@ -1,1 +1,24 @@
 <?php
+
+    /*######
+      *  CRUD de marcas
+     * */
+
+    function listarMarcas() : mysqli_result | bool
+    {
+        $link = conectar();
+        $sql = "SELECT * 
+                    FROM marcas 
+                    ORDER BY idMarca";
+        $resultado = mysqli_query( $link, $sql );
+        return $resultado;
+    }
+
+
+/*
+ *  listarMarcas()
+    verMarcaPorID()
+    agregarMarca()
+    modificarMarca()
+    eliminarMarca()
+ * */
