15b526dd45892ce172d977528d4caf37080c86a6
Marcos <exegeses@gmail.com>
2023-09-25 22:02:53 -0300

    Publicación de sitio

--- curso/catalogo/funciones/conexion.php
+++ curso/catalogo/funciones/conexion.php
@@ -1,9 +1,9 @@
 <?php
 
     const SERVER    = 'localhost';
-    const USUARIO   = 'root';
-    const CLAVE     = 'root';
-    const BASE      = 'catalogo63720';
+    const USUARIO   = 'root';//'id21312485_catalogo';
+    const CLAVE     = 'root';//'Catalogo-1';
+    const BASE      = 'catalogo63720';//'id21312485_catalogo';
 
     function conectar() : mysqli
     {
